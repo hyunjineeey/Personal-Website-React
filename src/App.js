d9dddaa70bbfe6b2e71714c2763f6f8f9b735505
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-17 22:20:16 -0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
-import Main from './Main'
+import TopAppBar from './components/TopAppBar'
+import Main from './components/Main'
+import About from './components/About'
+import Blog from './components/Blog'
+import Testimonials from './components/Testimonials'
+import Faq from './components/Faq'
 
 function App () {
   return (
-    <div className='App'>
-      <Main />
+    <div>
+      <TopAppBar />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='About' element={<About />} />
+        <Route path='Blog' element={<Blog />} />
+        <Route path='Testimonials' element={<Testimonials />} />
+        <Route path='Faq' element={<Faq />} />
+      </Routes>
     </div>
   )
 }
