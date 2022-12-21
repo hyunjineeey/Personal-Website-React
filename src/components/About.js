cfa1d9f110f83b0c04fe29222b35dc66ceab47bc
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-20 19:18:53 -0600

    Add Interests to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Box from '@mui/material/Box'
 import { Typography } from '@mui/material'
 import Button from '@mui/material/Button'
@@ -6,38 +7,41 @@ import SearchIcon from '@mui/icons-material/Search'
 
 import OppositeContentTimeline from './OppositeContentTimeline'
 import RowAndColumnSpacing from './RowAndColumnSpacing'
+import Interests from './Interests'
 import Footer from './Footer'
 
+const useStyles = makeStyles({
+  title: {
+    fontSize: '20px',
+    letterSpacing: '3px',
+    color: '#134db6',
+    textTransform: 'uppercase'
+  }
+})
+
 const About = () => {
+  const classes = useStyles()
+
   return (
     <Box sx={{ m: 2, textAlign: 'center' }}>
-      <Typography
-        sx={{
-          m: 2,
-          fontSize: '20px',
-          letterSpacing: '3px',
-          color: '#4b7dc4'
-        }}
-      >
+      <Typography className={classes.title}>
         Experience / Education
       </Typography>
       <OppositeContentTimeline />
       <Button variant='outlined' href='../resume.pdf' startIcon={<SearchIcon />}>
         Resume
       </Button>
-
       <Box sx={{ m: 6 }}>
-        <Typography
-          sx={{
-            m: 2,
-            fontSize: '20px',
-            letterSpacing: '3px',
-            color: '#4b7dc4'
-          }}
-        >
+        <Typography sx={{ mb: 4 }} className={classes.title}>
           Skills
         </Typography>
         <RowAndColumnSpacing />
+      </Box>
+      <Box sx={{ m: 6 }}>
+        <Typography sx={{ mb: 4 }} className={classes.title}>
+          Interests
+        </Typography>
+        <Interests />
       </Box>
       <Footer />
     </Box>
---
+++ src/components/Interests.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(0)
+  },
+  card: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    border: '1px solid black',
+    borderRadius: '5px',
+    textAlign: 'center'
+  },
+  icon: {
+    fontSize: '40px',
+    padding: theme.spacing(2, 0)
+  },
+  title: {
+    padding: theme.spacing(2)
+  },
+  featureList: {
+    padding: theme.spacing(1)
+  }
+}))
+
+const Interests = () => {
+  const classes = useStyles()
+
+  return (
+    <Container component='section' maxWidth='lg' className={classes.root}>
+      <Grid container spacing={3} alignItems='stretch'>
+        <Grid item xs={12} sm={4}>
+          <div className={classes.card}>
+            <FontAwesomeIcon className={classes.icon} icon='fa-solid fa-book' color='#bb382a' />
+            <Typography variant='h5' component='h3' className={classes.title}>
+              Reading / Writing
+            </Typography>
+            <Typography className={classes.featureList}>
+              I like to read and write.
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className={classes.card}>
+            <FontAwesomeIcon className={classes.icon} icon='fa-solid fa-camera-retro' color='#3bb665' />
+            <Typography variant='h5' component='h3' className={classes.title}>
+              Making a video
+            </Typography>
+            <Typography className={classes.featureList}>
+              I enjoy filming my life.
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className={classes.card}>
+            <FontAwesomeIcon className={classes.icon} icon='fa-solid fa-bolt' color='#f3b73b' />
+            <Typography variant='h5' component='h3' className={classes.title}>
+              Explore new things
+            </Typography>
+            <Typography className={classes.featureList}>
+              I am currently taking dance and drum classes!
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default Interests
