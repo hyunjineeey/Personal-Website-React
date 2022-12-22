88f5351d293360bf7ff86372a32f520fb8dd3d18
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-21 23:04:17 -0600

    Edit icons from main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,24 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import IconButton from '@mui/material/IconButton'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import YouTubeIcon from '@mui/icons-material/YouTube'
 import { Typography } from '@mui/material'
+import { makeStyles } from '@material-ui/core/styles'
 import Divider from '@mui/material/Divider'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import MainAvatar from './MainAvatar'
 
+const useStyles = makeStyles({
+  circleIcon: {
+    backgroundColor: '#ffc0c0',
+    padding: '10px',
+    borderRadius: '50%'
+  }
+})
+
 function Main () {
+  const classes = useStyles()
   return (
     <div>
       <Box
@@ -34,17 +44,15 @@ function Main () {
         <Typography>Born and Raised in Seoul, South Korea</Typography>
         <Box
           display='flex'
-          alignItems='center'
+          flexDirection='row'
           justifyContent='center'
           gap={1}
           sx={{ mt: 3 }}
         >
-          <Button variant='outlined' href='https://github.com/hyunjineeey' target='_blank'>
-            Github
-          </Button>
-          <Button variant='outlined' href='mailto:hyunjineeey@gmail.com'>
-            Email
-          </Button>
+            <Button variant='outlined' size='small'><FontAwesomeIcon icon='fa-solid fa-envelope' />Email</Button>
+            <Button variant='outlined' color='success' size='small'><FontAwesomeIcon icon='fa-brands fa-github' />Github</Button>
+            <Button variant='outlined' size='small'><FontAwesomeIcon icon='fa-brands fa-linkedin' />LinkedIn</Button>
+            <Button variant='outlined' color='error' size='small'><FontAwesomeIcon icon='fa-brands fa-youtube' />Youtube</Button>
         </Box>
         <Box
           display='flex'
@@ -53,10 +61,16 @@ function Main () {
           sx={{ pt: 2 }}
         >
           <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
-            <LinkedInIcon />
+            <FontAwesomeIcon icon='fa-solid fa-envelope' />
+          </IconButton>
+          <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
+            <FontAwesomeIcon icon='fa-brands fa-github' />
+          </IconButton>
+          <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
+            <FontAwesomeIcon icon='fa-brands fa-linkedin-in' />
           </IconButton>
           <IconButton color='error' href='https://www.youtube.com/@hyunjineeey' target='_blank'>
-            <YouTubeIcon />
+            <FontAwesomeIcon icon='fa-brands fa-youtube' />
           </IconButton>
         </Box>
       </Box>
@@ -68,5 +82,10 @@ function Main () {
     </div>
   )
 }
-
+// <Button variant='outlined' href='https://github.com/hyunjineeey' target='_blank'>
+//   Github
+// </Button>
+// <Button variant='outlined' href='mailto:hyunjineeey@gmail.com'>
+//   Email
+// </Button>
 export default Main
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,7 +3,7 @@ import { Stack } from '@mui/system'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
-let p = require('./project.json');
+const p = require('./project.json')
 
 const Project = ({ title, subtitle }) => {
   return (
@@ -16,50 +16,50 @@ const Project = ({ title, subtitle }) => {
 
 export default function ProjectItem () {
   return (
-      <Box
-        sx={{ width: '100%' }}
-        spacing={{ xs: 1, sm: 2, md: 3 }}
-      >
-        <Project
-          title={p.foodTruckFinder.title}
-          subtitle={p.foodTruckFinder.sub}
-        />
-        <Project
-          title={p.covid19.title}
-          subtitle={p.covid19.sub}
-        />
-        <Project
-          title={p.syntheticMonitoring.title}
-          subtitle={p.syntheticMonitoring.sub}
-        />
-        <Project
-          title={p.multithreadedMapReduce.title}
-          subtitle={p.multithreadedMapReduce.sub}
-        />
-        <Project
-          title={p.socketProgramming.title}
-          subtitle={p.socketProgramming.sub}
-        />
-        <Project
-          title={p.chatServer.title}
-          subtitle={p.chatServer.sub}
-        />
-        <Project
-          title={p.personalWebsite.title}
-          subtitle={p.personalWebsite.sub}
-        />
-        <Project
-          title={p.eventFilter.title}
-          subtitle={p.eventFilter.sub}
-        />
-        <Project
-          title={p.userControlSystem.title}
-          subtitle={p.userControlSystem.sub}
-        />
-        <Project
-          title={p.documentSimilarity.title}
-          subtitle={p.documentSimilarity.sub}
-        />
-      </Box>
+    <Box
+      sx={{ width: '100%' }}
+      spacing={{ xs: 1, sm: 2, md: 3 }}
+    >
+      <Project
+        title={p.foodTruckFinder.title}
+        subtitle={p.foodTruckFinder.sub}
+      />
+      <Project
+        title={p.covid19.title}
+        subtitle={p.covid19.sub}
+      />
+      <Project
+        title={p.syntheticMonitoring.title}
+        subtitle={p.syntheticMonitoring.sub}
+      />
+      <Project
+        title={p.multithreadedMapReduce.title}
+        subtitle={p.multithreadedMapReduce.sub}
+      />
+      <Project
+        title={p.socketProgramming.title}
+        subtitle={p.socketProgramming.sub}
+      />
+      <Project
+        title={p.chatServer.title}
+        subtitle={p.chatServer.sub}
+      />
+      <Project
+        title={p.personalWebsite.title}
+        subtitle={p.personalWebsite.sub}
+      />
+      <Project
+        title={p.eventFilter.title}
+        subtitle={p.eventFilter.sub}
+      />
+      <Project
+        title={p.userControlSystem.title}
+        subtitle={p.userControlSystem.sub}
+      />
+      <Project
+        title={p.documentSimilarity.title}
+        subtitle={p.documentSimilarity.sub}
+      />
+    </Box>
   )
 }
