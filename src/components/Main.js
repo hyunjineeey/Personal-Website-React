ccbf7b05b19bdb8605b2ed7dca45a88086f5d8e9
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-21 23:39:44 -0600

    Main page icons update

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,6 +17,11 @@ const useStyles = makeStyles({
   }
 })
 
+const email = 'mailto:hyunjineeey@gmail.com'
+const github = 'https://github.com/hyunjineeey'
+const linkedIn = 'https://www.linkedin.com/in/hyunjineeey/'
+const youtube = 'https://www.youtube.com/@hyunjineeey'
+
 function Main () {
   const classes = useStyles()
   return (
@@ -44,35 +49,26 @@ function Main () {
         <Typography>Born and Raised in Seoul, South Korea</Typography>
         <Box
           display='flex'
-          flexDirection='row'
-          justifyContent='center'
           gap={1}
           sx={{ mt: 3 }}
         >
-            <Button variant='outlined' size='small'><FontAwesomeIcon icon='fa-solid fa-envelope' />Email</Button>
-            <Button variant='outlined' color='success' size='small'><FontAwesomeIcon icon='fa-brands fa-github' />Github</Button>
-            <Button variant='outlined' size='small'><FontAwesomeIcon icon='fa-brands fa-linkedin' />LinkedIn</Button>
-            <Button variant='outlined' color='error' size='small'><FontAwesomeIcon icon='fa-brands fa-youtube' />Youtube</Button>
+          <Button href={email} sx={{ width: 100 }} target='_blank' variant='outlined' color='warning' size='small'><FontAwesomeIcon icon='fa-solid fa-envelope' />Email</Button>
+          <Button href={github} sx={{ width: 100 }} target='_blank' variant='outlined' color='success' size='small'><FontAwesomeIcon icon='fa-brands fa-github' />Github</Button>
+        </Box>
+        <Box
+          display='flex'
+          gap={1}
+          sx={{ mt: 1 }}
+        >
+          <Button href={linkedIn} sx={{ width: 100 }} target='_blank' variant='outlined' color='secondary' size='small'><FontAwesomeIcon icon='fa-brands fa-linkedin' />LinkedIn</Button>
+          <Button href={youtube} sx={{ width: 100 }} target='_blank' variant='outlined' color='error' size='small'><FontAwesomeIcon icon='fa-brands fa-youtube' />Youtube</Button>
         </Box>
         <Box
           display='flex'
           alignItems='center'
           justifyContent='center'
           sx={{ pt: 2 }}
-        >
-          <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
-            <FontAwesomeIcon icon='fa-solid fa-envelope' />
-          </IconButton>
-          <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
-            <FontAwesomeIcon icon='fa-brands fa-github' />
-          </IconButton>
-          <IconButton color='primary' href='https://www.linkedin.com/in/hyunjineeey/' target='_blank'>
-            <FontAwesomeIcon icon='fa-brands fa-linkedin-in' />
-          </IconButton>
-          <IconButton color='error' href='https://www.youtube.com/@hyunjineeey' target='_blank'>
-            <FontAwesomeIcon icon='fa-brands fa-youtube' />
-          </IconButton>
-        </Box>
+        />
       </Box>
       <Box>
         <Divider sx={{ borderBottomWidth: 1, mb: 1 }} />
