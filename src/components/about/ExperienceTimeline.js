260a977402df87a738de3521767e19a36bfdc543
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-23 18:41:54 -0600

    Timeline clean up

--- src/components/about/ExperienceTimeline.js
+++ src/components/about/ExperienceTimeline.js
@@ -10,118 +10,88 @@ import Link from '@mui/material/Link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function ExperienceTimeline () {
+  const Item = ({ date, variant, color, company, title, link, icon }) => {
+    return (
+      <TimelineItem >
+        <TimelineOppositeContent sx={{ fontSize: "0.9rem" }} >
+          {date}
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          {icon
+            ? <TimelineDot color={color}>
+              <FontAwesomeIcon size='xs' icon='fa-solid fa-graduation-cap' />
+              </TimelineDot>
+            : <TimelineDot variant={variant} color={color} />}
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Link href={link} underline='none' target='_blank' rel='noreferrer'>{company}</Link>
+          <br />{title}
+        </TimelineContent>
+      </TimelineItem>
+    )
+  }
   return (
-    <Timeline>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Oct 2021 - Dec 2022
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot variant='filled' color='error' />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://sentera.com/' underline='none' target='_blank' rel='noreferrer'>Sentera</Link>
-          <br />Frontend Engineer
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Graduated Dec 2020
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color='warning'>
-            <FontAwesomeIcon icon='fa-solid fa-graduation-cap' />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://cse.umn.edu/college/computer-science' underline='none' target='_blank' rel='noreferrer'>University of Minnesota</Link>
-          <br />Computer Science
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Completed Oct 2020
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color='success'>
-            <FontAwesomeIcon icon='fa-brands fa-free-code-camp' />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://www.udemy.com/course/modern-react-bootcamp/' underline='none' target='_blank' rel='noreferrer'>Udemy</Link>
-          <br />The Modern React Bootcamp
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          May - Aug 2020
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot variant='outlined' />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://www.smartthings.com/' underline='none' target='_blank' rel='noreferrer'>SmartThings</Link>
-          <br />Software Engineer Intern
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Nov 2019 - May 2020
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot variant='outlined' />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://flywheel.io/' underline='none' target='_blank' rel='noreferrer'>Flywheel</Link>
-          <br />Software Engineer Intern
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          May - Dec 2019
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot variant='outlined' />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://twin-cities.umn.edu/' underline='none' target='_blank' rel='noreferrer'>University of Minnesota</Link>
-          <br />Student Web Support
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Completed July 2019
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color='success'>
-            <FontAwesomeIcon icon='fa-brands fa-free-code-camp' />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://www.udemy.com/course/the-web-developer-bootcamp/' underline='none' target='_blank' rel='noreferrer'>Udemy</Link>
-          <br />The Web Developer Bootcamp
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color='text.secondary'>
-          Feb - Jun 2019
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot variant='outlined' />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Link href='https://www.childrenstorytime.co/' underline='none' target='_blank' rel='noreferrer'>Children Story Time</Link>
-          <br />Frontend Engineer Intern
-        </TimelineContent>
-      </TimelineItem>
+    <Timeline sx={{ p: 0 }}>
+      <Item
+        date='Oct 2021 - Dec 2022'
+        variant='filled'
+        company='Sentera'
+        title='Frontend Engineer'
+        link='https://sentera.com/'
+      />
+      <Item
+        date='Graduated Dec 2020'
+        color='warning'
+        company='University of Minnesota'
+        title='Computer Science'
+        link='https://cse.umn.edu/college/computer-science'
+        icon='fa-solid fa-graduation-cap'
+      />
+      <Item
+        date='Completed Oct 2020'
+        color='success'
+        company='Udemy'
+        title='The Modern React Bootcamp'
+        link='https://www.udemy.com/course/modern-react-bootcamp/'
+        icon='fa-brands fa-free-code-camp'
+      />
+      <Item
+        date='May - Aug 2020'
+        variant='outlined'
+        company='SmartThings'
+        title='Software Engineer Intern'
+        link='https://www.smartthings.com/'
+      />
+      <Item
+        date='Nov 2019 - May 2020'
+        variant='outlined'
+        company='Flywheel'
+        title='Software Engineer Intern'
+        link='https://flywheel.io/'
+      />
+      <Item
+        date='May - Dec 2019'
+        variant='outlined'
+        company='University of Minnesota'
+        title='Student Web Support'
+        link='https://twin-cities.umn.edu/'
+      />
+      <Item
+        date='Completed July 2019'
+        color='success'
+        company='Udemy'
+        title='The Web Developer Bootcamp'
+        link='https://www.udemy.com/course/the-web-developer-bootcamp/'
+        icon='fa-brands fa-free-code-camp'
+      />
+      <Item
+        date='Feb - Jun 2019'
+        variant='outlined'
+        company='Children Story Time'
+        title='Frontend Engineer Intern'
+        link='https://www.childrenstorytime.co/'
+      />
     </Timeline>
   )
 }
