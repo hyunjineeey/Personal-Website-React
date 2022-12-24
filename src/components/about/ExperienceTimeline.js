25cc29874a655e666c1336c76ec7f733e429f45b
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-24 16:34:19 -0600

    lint

--- src/components/about/ExperienceTimeline.js
+++ src/components/about/ExperienceTimeline.js
@@ -1,106 +1,108 @@
-import * as React from 'react'
-import Timeline from '@mui/lab/Timeline'
-import TimelineItem from '@mui/lab/TimelineItem'
-import TimelineSeparator from '@mui/lab/TimelineSeparator'
-import TimelineConnector from '@mui/lab/TimelineConnector'
-import TimelineContent from '@mui/lab/TimelineContent'
-import TimelineDot from '@mui/lab/TimelineDot'
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent'
-import Link from '@mui/material/Link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useTranslation } from 'react-i18next'
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import Link from "@mui/material/Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslation } from "react-i18next";
 
-export default function ExperienceTimeline () {
-  const { t } = useTranslation(['about'])
+export default function ExperienceTimeline() {
+  const { t } = useTranslation(["about"]);
   const Item = ({ date, variant, color, company, title, link, icon }) => {
     return (
       <TimelineItem>
-        <TimelineOppositeContent sx={{ fontSize: '0.9rem' }}>
+        <TimelineOppositeContent
+          sx={{ fontSize: "0.9rem" }}
+          align="right"
+          variant="body3"
+        >
           {date}
         </TimelineOppositeContent>
         <TimelineSeparator>
-          {icon
-            ? (
-              <TimelineDot color={color}>
-                <FontAwesomeIcon size='xs' icon='fa-solid fa-graduation-cap' />
-              </TimelineDot>
-              )
-            : (
-              <TimelineDot variant={variant} color={color} />
-              )}
+          {icon ? (
+            <TimelineDot color={color}>
+              <FontAwesomeIcon size="xs" icon="fa-solid fa-graduation-cap" />
+            </TimelineDot>
+          ) : (
+            <TimelineDot variant={variant} color={color} />
+          )}
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-          <Link href={link} underline='none' target='_blank' rel='noreferrer'>
+          <Link href={link} underline="none" target="_blank" rel="noreferrer">
             {company}
           </Link>
           <br />
           {title}
         </TimelineContent>
       </TimelineItem>
-    )
-  }
+    );
+  };
   return (
     <Timeline sx={{ p: 0 }}>
       <Item
-        date={t('about:sentera.date')}
-        variant='filled'
-        company='Sentera'
-        title={t('about:sentera.title')}
-        link='https://sentera.com/'
+        date={t("about:sentera.date")}
+        variant="filled"
+        company="Sentera"
+        title={t("about:sentera.title")}
+        link="https://sentera.com/"
       />
       <Item
-        date={t('about:umnSchool.date')}
-        color='warning'
-        company='University of Minnesota'
-        title={t('about:umnSchool.title')}
-        link='https://cse.umn.edu/college/computer-science'
-        icon='fa-solid fa-graduation-cap'
+        date={t("about:umnSchool.date")}
+        color="warning"
+        company="University of Minnesota"
+        title={t("about:umnSchool.title")}
+        link="https://cse.umn.edu/college/computer-science"
+        icon="fa-solid fa-graduation-cap"
       />
       <Item
-        date={t('about:udemyReact.date')}
-        color='success'
-        company='Udemy'
-        title={t('about:udemyReact.title')}
-        link='https://www.udemy.com/course/modern-react-bootcamp/'
-        icon='fa-brands fa-free-code-camp'
+        date={t("about:udemyReact.date")}
+        color="success"
+        company="Udemy"
+        title={t("about:udemyReact.title")}
+        link="https://www.udemy.com/course/modern-react-bootcamp/"
+        icon="fa-brands fa-free-code-camp"
       />
       <Item
-        date={t('about:smartThings.date')}
-        variant='outlined'
-        company='SmartThings'
-        title={t('about:softwareEngineerIntern')}
-        link='https://www.smartthings.com/'
+        date={t("about:smartThings.date")}
+        variant="outlined"
+        company="SmartThings"
+        title={t("about:softwareEngineerIntern")}
+        link="https://www.smartthings.com/"
       />
       <Item
-        date={t('about:flywheel.date')}
-        variant='outlined'
-        company='Flywheel'
-        title={t('about:softwareEngineerIntern')}
-        link='https://flywheel.io/'
+        date={t("about:flywheel.date")}
+        variant="outlined"
+        company="Flywheel"
+        title={t("about:softwareEngineerIntern")}
+        link="https://flywheel.io/"
       />
       <Item
-        date={t('about:umnWork.date')}
-        variant='outlined'
-        company='University of Minnesota'
-        title={t('about:umnWork.title')}
-        link='https://twin-cities.umn.edu/'
+        date={t("about:umnWork.date")}
+        variant="outlined"
+        company="University of Minnesota"
+        title={t("about:umnWork.title")}
+        link="https://twin-cities.umn.edu/"
       />
       <Item
-        date={t('about:udemyWeb.date')}
-        color='success'
-        company='Udemy'
-        title={t('about:udemyWeb.title')}
-        link='https://www.udemy.com/course/the-web-developer-bootcamp/'
-        icon='fa-brands fa-free-code-camp'
+        date={t("about:udemyWeb.date")}
+        color="success"
+        company="Udemy"
+        title={t("about:udemyWeb.title")}
+        link="https://www.udemy.com/course/the-web-developer-bootcamp/"
+        icon="fa-brands fa-free-code-camp"
       />
       <Item
-        date={t('about:childrenStoryTime.date')}
-        variant='outlined'
-        company='Children Story Time'
-        title={t('about:childrenStoryTime.title')}
-        link='https://www.childrenstorytime.co/'
+        date={t("about:childrenStoryTime.date")}
+        variant="outlined"
+        company="Children Story Time"
+        title={t("about:childrenStoryTime.title")}
+        link="https://www.childrenstorytime.co/"
       />
     </Timeline>
-  )
+  );
 }
