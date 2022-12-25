bc71a7cd9ec59b94129b27e2bcf1e06b6648e647
Hyunjin Jae <hyunjineeey@gmail.com>
2022-12-24 20:44:36 -0600

    Remove tooltip from flag

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,100 +1,100 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
-import MenuIcon from '@mui/icons-material/Menu'
-import Container from '@mui/material/Container'
-import Button from '@mui/material/Button'
-import Tooltip from '@mui/material/Tooltip'
-import MenuItem from '@mui/material/MenuItem'
-import { Link } from 'react-router-dom'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
 
-import '/node_modules/flag-icons/css/flag-icons.min.css'
-import i18n from '../i18n'
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import i18n from "../i18n";
 
-function TopAppBar () {
-  const pages = ['About', 'Projects', 'Testimonials']
-  const [anchorElNav, setAnchorElNav] = React.useState(null)
+function TopAppBar() {
+  const pages = ["About", "Projects", "Testimonials"];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   //  const [anchorElUser, setAnchorElUser] = React.useState(null)
 
   const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget)
-  }
+    setAnchorElNav(event.currentTarget);
+  };
   /*  const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget)
   }
 */
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
+    setAnchorElNav(null);
+  };
 
   /*  const handleCloseUserMenu = () => {
     setAnchorElUser(null)
   }
 */
   const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng)
-    localStorage.setItem('lng', lng)
-  }
+    i18n.changeLanguage(lng);
+    localStorage.setItem("lng", lng);
+  };
   return (
-    <AppBar position='fixed'>
-      <Container maxWidth='xl'>
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
-            variant='h6'
+            variant="h6"
             noWrap
-            component='a'
-            href='/'
+            component="a"
+            href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Hyunjin Jae
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color='inherit'
+              color="inherit"
             >
               <MenuIcon />
             </IconButton>
             <Menu
-              id='menu-appbar'
+              id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' }
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign='center'>
+                  <Typography textAlign="center">
                     <Link
-                      style={{ textDecoration: 'none', color: 'black' }}
+                      style={{ textDecoration: "none", color: "black" }}
                       to={`/${page}`}
                     >
                       {page}
@@ -105,32 +105,32 @@ function TopAppBar () {
             </Menu>
           </Box>
           <Typography
-            variant='h5'
+            variant="h5"
             noWrap
-            component='a'
-            href='/'
+            component="a"
+            href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Hyunjin
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 <Link
-                  style={{ textDecoration: 'none', color: 'white' }}
+                  style={{ textDecoration: "none", color: "white" }}
                   to={`/${page}`}
                 >
                   {page}
@@ -140,23 +140,18 @@ function TopAppBar () {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title='Switch language'>
-              <IconButton size='small' sx={{ p: 0 }}>
-                <div
-                  onClick={() => changeLanguage('kr')}
-                  className='fi fi-kr'
-                  style={{ marginRight: 5 }}
-                />
-                <div
-                  onClick={() => changeLanguage('us')}
-                  className='fi fi-us'
-                />
-              </IconButton>
-            </Tooltip>
+            <IconButton size="small" sx={{ p: 0 }}>
+              <div
+                onClick={() => changeLanguage("kr")}
+                className="fi fi-kr"
+                style={{ marginRight: 5 }}
+              />
+              <div onClick={() => changeLanguage("us")} className="fi fi-us" />
+            </IconButton>
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
-  )
+  );
 }
-export default TopAppBar
+export default TopAppBar;
