714094d00c9e653e849a53472753f200f5ce36bb
Hyunjin Jae <hyunjineeey@gmail.com>
2023-01-03 17:50:08 -0600

    Add last updated to FAQ

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import { useTranslation } from 'react-i18next'
+import { Typography } from '@mui/material'
 
 import Footer from '../Footer'
 import Accordion from './FaqAccordion'
 import QnATable from './QnATable'
 
 const Faq = () => {
+  const { t } = useTranslation()
+
   return (
     <Box sx={{ textAlign: 'center' }}>
       <Accordion />
       <QnATable />
+      <Box sx={{ ml: 3, mb: 2, mt: -1 }}>
+        <Typography
+          variant='subtitle2'
+          sx={{ textAlign: 'left', fontStyle: 'italic' }}
+        >
+          {t('shared:lastUpdated')}: {t('shared:faqLastUpdated')}
+        </Typography>
+      </Box>
       <Footer />
     </Box>
   )
