973947bc4714025fc8f6aca52997d9bcbc53a47b
Hyunjin Jae <hyunjineeey@gmail.com>
2023-01-06 01:50:44 -0600

    Fix Carousel dots

--- src/components/writing/Carousel.js
+++ src/components/writing/Carousel.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useSwipeable } from 'react-swipeable'
-import Box from '@mui/material/Box'
-import { map } from 'lodash'
 import Typography from '@mui/material/Typography'
-import Pagination from '@mui/material/Pagination'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { size } from 'lodash'
 
 import './Carousel.css'
 
 export const CarouselItem = ({ children, width }) => {
-  const items = children?.split('\b')
-
   return (
     <Box
       className='carousel-item'
@@ -25,7 +25,7 @@ export const CarouselItem = ({ children, width }) => {
   )
 }
 
-const Carousel = ({ children }) => {
+const Carousel = ({ children, writing }) => {
   const [activeIndex, setActiveIndex] = useState(0)
 
   const updateIndex = (newIndex) => {
@@ -43,49 +43,48 @@ const Carousel = ({ children }) => {
     onSwipedRight: () => updateIndex(activeIndex - 1)
   })
 
+  const arrowStyle = {
+    background: 'transparent',
+    border: 0,
+    color: '#424242'
+  }
+
   return (
-    <div
-      {...handlers}
-      className='carousel'
-      //   onMouseEnter={() => setPaused(true)}
-      //   onMouseLeave={() => setPaused(false)}
-    >
+    <div {...handlers} className='carousel'>
+      <div className='indicators'>
+        <Typography sx={{ textAlign: 'center', alignContent: 'center' }}>
+          최신
+        </Typography>
+        <Button style={arrowStyle}>
+          <ArrowBackIcon
+            onClick={() => {
+              updateIndex(activeIndex - 1)
+            }}
+            style={{ fontSize: '15px' }}
+          />
+        </Button>
+        <Typography sx={{ textAlign: 'center', alignContent: 'center' }}>
+          {activeIndex + 1} / {size(writing.lines)}
+        </Typography>
+        <Button style={arrowStyle}>
+          <ArrowForwardIcon
+            onClick={() => {
+              updateIndex(activeIndex + 1)
+            }}
+            style={{ fontSize: '15px' }}
+          />
+        </Button>
+        <Typography sx={{ textAlign: 'center', alignContent: 'center' }}>
+          예전
+        </Typography>
+      </div>
       <div
         className='inner'
         style={{ transform: `translateX(-${activeIndex * 100}%)` }}
       >
-        {map(children, (child, index) => {
+        {React.Children.map(children, (child, index) => {
           return React.cloneElement(child, { width: '100%' })
         })}
-      </div>
-
-      <div className='indicators'>
-        <button
-          onClick={() => {
-            updateIndex(activeIndex - 1)
-          }}
-        >
-          Prev
-        </button>
-        {map(children, (child, index) => {
-          return (
-            <button
-              className={`${index === activeIndex ? 'active' : ''}`}
-              onClick={() => {
-                updateIndex(index)
-              }}
-            >
-              {index + 1}
-            </button>
-          )
-        })}
-        <button
-          onClick={() => {
-            updateIndex(activeIndex + 1)
-          }}
-        >
-          Next
-        </button>
       </div>
     </div>
   )
--- src/components/writing/Writing.js
+++ src/components/writing/Writing.js
@@ -1,154 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { map, size, range } from 'lodash'
-import Carousel from 'react-multi-carousel'
-import Button from '@mui/material/Button'
-import Pagination from '@mui/material/Pagination'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-import { getOriginalCounterPart } from 'react-multi-carousel/lib/utils'
+import { map } from 'lodash'
+import Carousel, { CarouselItem } from './Carousel'
 
 import 'react-multi-carousel/lib/styles.css'
-import './Carousel.css'
 
 import Footer from '../Footer'
 const writing = require('./writing.json')
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 1
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 1
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-}
-
-const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
-  const {
-    carouselState: { currentSlide }
-  } = rest
-  return (
-    <div className='carousel-button-group'>
-      <Button
-        className={currentSlide === 0 ? 'disable' : ''}
-        onClick={() => previous()}
-        variant='contained'
-        size='small'
-        sx={{ mr: 2, mt: 2 }}
-      >
-        Prev
-      </Button>
-      <Button
-        variant='contained'
-        size='small'
-        onClick={() => next()}
-        sx={{ mt: 2 }}
-      >
-        Next
-      </Button>
-    </div>
-  )
-}
-
-const arrowStyle = {
-  borderRadius: '50%',
-  //   background: 'rgb(180, 180, 180)',
-  background: 'transparent',
-  border: 0,
-  color: '#424242'
-}
-
-const CustomRight = ({ onClick }) => (
-  <button className='arrow right' onClick={onClick} style={arrowStyle}>
-    <ArrowForwardIcon style={{ fontSize: '13px' }} />
-  </button>
-)
-const CustomLeft = ({ onClick }) => (
-  <button className='arrow left' onClick={onClick} style={arrowStyle}>
-    <ArrowBackIcon style={{ fontSize: '13px' }} />
-  </button>
-)
-
-const CustomDot = ({ onMove, index, onClick, active }) => {
-  // onMove means if dragging or swiping in progress.
-  // active is provided by this lib for checking if the item is active or not.
-  return (
-    <li className={active ? 'active' : 'inactive'} onClick={() => onClick()}>
-      {index + 1}
-    </li>
-  )
-}
-
 const Writing = () => {
-  const [current, setCurrent] = useState(null)
-  const [count, setCount] = useState(1)
   return (
     <Box sx={{ textAlign: 'center' }}>
-      <Box sx={{ width: '100%' }}>
-        <Carousel
-          //   showDots
-          ref={(el) => {
-            if (el) {
-              var slidesToShow = el.state.slidesToShow
-              var currentSlide = el.state.currentSlide - 1
-
-              setCount(currentSlide)
-              setCurrent(slidesToShow + currentSlide)
-            }
-          }}
-          responsive={responsive}
-          infinite
-          dotListClass='custom-dot-list-style'
-          arrows
-          customRightArrow={<CustomRight />}
-          customLeftArrow={<CustomLeft />}
-          containerClass='carousel-with-custom-dots'
-          beforeChange={(nextSlide, { currentSlide, onMove }) => {
-            setCount(currentSlide)
-            console.log(range(count))
-          }}
-          //   customDot={<CustomDot />}
-          //   customButtonGroup={<ButtonGroup />}
-          //   renderButtonGroupOutside
-        >
+      <Box sx={{ ml: 3, mr: 3 }}>
+        <Carousel writing={writing}>
           {map(writing.lines, (line) => (
-            <Box
-              sx={{
-                ml: 4,
-                mr: 4,
-                mb: 4,
-                textAlign: 'center'
-              }}
-            >
-              <Typography
-                sx={{
-                  whiteSpace: 'pre-wrap',
-                  lineHeight: '150%',
-                  display: 'inline-block',
-                  textAlign: 'left',
-                  fontSize: '15px'
-                }}
-              >
-                {line}
-              </Typography>
-            </Box>
+            <CarouselItem>{line}</CarouselItem>
           ))}
         </Carousel>
-        <p>
-          {count} / {size(writing.lines)}
-        </p>
       </Box>
 
       <Footer />
